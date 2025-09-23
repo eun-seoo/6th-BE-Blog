51b8e1e35f70fc5a6eed42bf467d440b56045b4f
eun-seoo <kimeunseo040@gamil.com>
2025-09-23 16:02:08 +0900

    feat: 프로젝트 초기 설정

---
+++ src/test/java/com/leets/backend/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leets.backend.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

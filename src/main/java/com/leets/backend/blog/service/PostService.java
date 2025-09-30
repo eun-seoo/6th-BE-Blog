893e7044578a8f31c67520b91f1f4814c7854e6e
eun-seoo <kimeunseo040@gamil.com>
2025-09-30 17:45:06 +0900

    chore: JPA ddl-auto 옵션 추가하여 테이블 자동 생성 설정

--- src/main/java/com/leets/backend/blog/service/PostService.java
+++ src/main/java/com/leets/backend/blog/service/PostService.java
@@ -1,31 +1,8 @@
 package com.leets.backend.blog.service;
 
-import com.leets.backend.blog.controller.dto.PostRequest;
-import com.leets.backend.blog.controller.dto.PostResponse;
-import com.leets.backend.blog.model.Post;
-import com.leets.backend.blog.repository.PostRepository;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 public class PostService {
-    private final PostRepository repository;
-
-    //의존성 주입(Service가 Repository라는 도구 사용할 수 있게 연결)
-    public PostService(PostRepository repository){
-        this.repository = repository;
-    }
-
-    //저장된 모든 글을 Repository에서 꺼내와서 Controller에게 반환하는 메서드
-    public List<Post> findAll(){
-        return repository.findAll();
-    }
-
-    //DB 저장 과정
-    public PostResponse save(PostRequest request){
-        Post post = new Post(request.getTitle(), request.getContent());
-        Post saved = repository.save(post);
-        return new PostResponse(saved.getId(), saved.getTitle(), saved.getContent());
-    }
+    // 3주차 과제에서 구현 예정
 }

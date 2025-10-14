9426878515a2ef693c301d674169e5b200ed89f5
eun-seoo <kimeunseo040@gamil.com>
2025-10-14 19:10:23 +0900

    feat: 게시글/유저 레포지토리 정의

--- src/main/java/com/leets/backend/blog/post/repository/PostRepository.java
+++ src/main/java/com/leets/backend/blog/post/repository/PostRepository.java
@@ -1,4 +1,7 @@
 package com.leets.backend.blog.post.repository;
 
-public class PostRepository {
+import com.leets.backend.blog.post.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
 }

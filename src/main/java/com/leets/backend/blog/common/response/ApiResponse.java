ed6ef84b4f15873358f97c216eb2ee9c19494a54
eun-seoo <kimeunseo040@gamil.com>
2025-10-14 19:01:50 +0900

    feat: 공통 응답 클래스 구현

--- src/main/java/com/leets/backend/blog/common/response/ApiResponse.java
+++ src/main/java/com/leets/backend/blog/common/response/ApiResponse.java
@@ -1,4 +1,25 @@
 package com.leets.backend.blog.common.response;
 
-public class ApiResponse {
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.springframework.http.HttpStatus;
+import com.leets.backend.blog.common.exception.*;
+
+public record ApiResponse<T>(
+        @JsonIgnore HttpStatus httpStatus,
+        boolean success,
+        T data,
+        ExceptionDto error
+) {
+    public static <T> ApiResponse<T> ok(T data) {
+        return new ApiResponse<>(HttpStatus.OK, true, data, null);
+    }
+
+    public static <T> ApiResponse<T> created(T data) {
+        return new ApiResponse<>(HttpStatus.CREATED, true, data, null);
+    }
+
+    public static <T> ApiResponse<T> fail(CustomException e) {
+        return new ApiResponse<>(e.getErrorCode().getHttpStatus(), false, null,
+                ExceptionDto.of(e.getErrorCode(), e.getErrorMsg()));
+    }
 }

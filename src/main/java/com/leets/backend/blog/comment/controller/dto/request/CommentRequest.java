341e5c3f0a33d16c38bf5232894f9fcc56bca77a
eun-seoo <kimeunseo040@gamil.com>
2025-10-28 17:27:14 +0900

    feat(comment): 댓글 작성 요청 DTO 추가

--- src/main/java/com/leets/backend/blog/comment/controller/dto/request/CommentRequest.java
+++ src/main/java/com/leets/backend/blog/comment/controller/dto/request/CommentRequest.java
@@ -1,4 +1,12 @@
 package com.leets.backend.blog.comment.controller.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+
 public class CommentRequest {
+    @NotBlank(message = "내용은 필수 입력 항목입니다.")
+    private String content;
+
+    public String getContent() {
+        return content;
+    }
 }

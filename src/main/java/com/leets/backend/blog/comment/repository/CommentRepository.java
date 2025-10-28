28f63ca28fe28b8726f23c0c52bf59dac05746f8
eun-seoo <kimeunseo040@gamil.com>
2025-10-28 14:27:35 +0900

    feat(comment): CommentRepository 구현

--- src/main/java/com/leets/backend/blog/comment/repository/CommentRepository.java
+++ src/main/java/com/leets/backend/blog/comment/repository/CommentRepository.java
@@ -1,4 +1,9 @@
 package com.leets.backend.blog.comment.repository;
 
-public class CommentRepository {
+import com.leets.backend.blog.comment.entity.Comment;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findByPostId(Long postId);
 }
